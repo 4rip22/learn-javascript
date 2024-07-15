527d139b41a706d5824c9550e841b179359676da
arip saputra <kkruipputra816@gmail.com>
2024-07-15 21:53:53 +0700

    learn code basic 1

---
+++ Date/GetMethod.js
@@ -0,0 +1,15 @@
+//get method
+const date = new Date("2024-08-22");
+date.getUTCFullYear();
+date.getUTCMonth();
+date.getUTCDate();
+
+
+//method getFullYear mengembalikan tahun
+console.log(date.getUTCFullYear());
+//method getMonth mengembalikan bulan berbasis 0
+console.log(date.getUTCMonth());
+//method getDate mengembalikan tanggal
+console.log(date.getUTCDate());
+
+console.log("tanggal ini lah saya Ulang Tahun :", date);
