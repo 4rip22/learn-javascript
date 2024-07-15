b30e18af601fe39559d95e28cc48b60baf4e4e49
arip saputra <kkruipputra816@gmail.com>
2024-07-15 20:32:58 +0700

    code dasar

---
+++ arraysort/arrayconst.js
@@ -0,0 +1,6 @@
+const nama_manusia = ["rifqi", "rival", "riki"];   
+    //mengganti nama_manusia di dalam array dengan bernama rifqi menjadi fuad 
+    nama_manusia[0] = "fuad";
+    //menambahkan array baru yang bernama aksan
+    nama_manusia.push('aksan');
+    console.log(nama_manusia);
---
+++ arraysort/arraysort.js
@@ -0,0 +1,15 @@
+//array sort ascending
+const hewan = ["harimau","bebek","angsa","kucing"];
+hewan.reverse();
+console.log(hewan);
+
+//output
+//[ 'kucing', 'angsa', 'bebek', 'harimau' ]
+
+//array sort descending
+const fruit = ["mangga","apel","pisang","anggur"];
+fruit.sort();
+console.log(fruit);
+
+//output
+//[ 'anggur', 'apel', 'mangga', 'pisang' ]
