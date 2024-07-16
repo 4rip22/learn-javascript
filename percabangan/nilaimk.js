adf6e55a510345a5c32b8c91d8a29fa367f5f151
arip saputra <kkruipputra816@gmail.com>
2024-07-16 18:53:26 +0700

    percabangan if else and switch

--- percabangan/nilaimk.js
+++ percabangan/nilaimk.js
@@ -1,9 +1,9 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 rl.question('Masukkan nilai matakuliah: ', (jawaban) => {
   const nilai = parseInt(jawaban);
