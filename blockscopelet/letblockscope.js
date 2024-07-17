3ebc21187c480ec365390542c4a53e675a2fb563
arip saputra <kkruipputra816@gmail.com>
2024-07-17 10:09:04 +0700

    learn let and loop

---
+++ blockscopelet/letblockscope.js
@@ -0,0 +1,6 @@
+//let block scope
+let nama = "arip";
+let nama = "saputra";
+
+console.log(nama); //output -> SyntaxError: Identifier 'nama' has already been declared
+
---
+++ loop/loop.js
@@ -0,0 +1,6 @@
+//looping dengan for loop
+var a = "hello world";
+
+for(var a = 0; a < 10; a++){
+
+}
