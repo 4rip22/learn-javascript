714aa468e1669e036c856c6dc91e4124d15d3334
arip saputra <kkruipputra816@gmail.com>
2024-07-22 09:33:31 +0700

    learn constructor

--- object/ObDestructuring.js
+++ object/ObDestructuring.js
@@ -19,10 +19,19 @@ const data2 = {
    
 };
 
+const data3 = {
+    firstName : "fuad",
+    lastName : "hamidan",
+    age : 19,
+    address : "Kedaton",
+    regency : "Bandar Lampung",
+    province : "lampung"
+};
 
 //membuat parameter dengan destructuring
 let {firstName, lastName, age,address,regency,province} = data1;
 let {firstName:firstName2, lastName:lastName2, age:age2,address:address2,regency:regency2,province:province2} = data2;
+let {firstName:firstName3, lastName:lastName3, age:age3,address:address3,regency:regency3,province:province3} = data3;
 
 console.log("------");
 console.log("DATA 1");
@@ -30,6 +39,7 @@ console.log("------");
 
 console.log("Nama :", data1.firstName, data1.lastName);
 console.log("Umur :",data1.age);
+console.log("Alamat :", data1 .address);
 console.log("Kabupaten :",data1.regency);
 console.log("Provinsi :",data1.province);
 
@@ -39,5 +49,16 @@ console.log("------");
 
 console.log("Nama :", data2.firstName, data2.lastName);
 console.log("Umur :",data2.age);
+console.log("Alamat :", data2 .address);
 console.log("Kabupaten :",data2.regency);
 console.log("Provinsi :",data2.province);
+
+console.log("------");
+console.log("DATA 3");
+console.log("------");
+
+console.log("Nama :", data3.firstName, data3.lastName);
+console.log("Umur :", data3.age);
+console.log("Alamat :", data3 .address);
+console.log("Kabupaten :", data3.regency);
+console.log("Provinsi :", data3.province);
