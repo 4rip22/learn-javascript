4d8d38c495f6c76bc9e9eda9dcef44b79d47324b
arip saputra <kkruipputra816@gmail.com>
2024-07-24 19:14:15 +0700

    DOM

---
+++ DOM/index.js
@@ -0,0 +1,8 @@
+const Hello = document.getElementById("Nama");
+Hello.innerHTML = "Nama saya adalah ARIP SAPUTRA";
+
+let color = document.getElementsByClassName("color-text");
+color[0].style.color = "white";
+
+let container = document.getElementsByClassName("container");
+container[0].style.backgroundColor = "blue";
