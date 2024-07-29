362ecc44d39f9324aa5c43444e3d437e29374d53
arip saputra <kkruipputra816@gmail.com>
2024-07-29 23:08:37 +0700

    learn code Arroe Function

--- javascriptLanjutan/ArrowFunction/higherOrderFun.js
+++ javascriptLanjutan/ArrowFunction/higherOrderFun.js
@@ -19,3 +19,15 @@ const number1 = [4,54,3,21,2,1,3,5,4,7];
 //menjumlahkan semua angka di dalam array
 const newNumber1 = number1.reduce((Value1, Value2) =>  Value1  + Value2 );
 console.log(newNumber1);
+
+
+//method chaning
+const numbers = [2,3,4,5,6,7,8,9,10];
+
+//cari angka > 4 
+//kalikan semua angka di dalam array
+//jumlahkan semua di dalam array
+
+//filter:5,6,7,8,9,10 map:10,12,14,16,18,20 reduce:70
+const hasil = numbers.filter(n => n > 4).map(n => n * 4).reduce((Value1, Value2) =>  Value1  + Value2 );
+console.log(hasil)
